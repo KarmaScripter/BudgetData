f2099442503561900c518ccf8f31e7740e4e6b64
Terry D. Eppler <terry.eppler@gmail.com>
2021-10-10 20:27:48 -0500

    ref

--- conversion/sdf/ForeignKeySchema.cs
+++ conversion/sdf/ForeignKeySchema.cs
@@ -4,18 +4,39 @@
 
 namespace BudgetExecution
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class ForeignKeySchema
     {
+        /// <summary>
+        /// The cascade on delete
+        /// </summary>
         public bool cascadeOnDelete;
 
+        /// <summary>
+        /// The column name
+        /// </summary>
         public string columnName;
 
+        /// <summary>
+        /// The foreign column name
+        /// </summary>
         public string foreignColumnName;
 
+        /// <summary>
+        /// The foreign table name
+        /// </summary>
         public string foreignTableName;
 
+        /// <summary>
+        /// The is nullable
+        /// </summary>
         public bool isNullable;
 
+        /// <summary>
+        /// The table name
+        /// </summary>
         public string tableName;
     }
 }
